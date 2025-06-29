573115d86ea0047ebf4b076f40cf40c0a397111b
dev <sempernow.com@gmail.com>
2025-06-29 18:54:51 -0400

    firewalld-halb.sh @ 2025-06-29T22:52Z

--- firewalld-halb.sh
+++ firewalld-halb.sh
@@ -1,57 +1,82 @@
 #!/usr/bin/env bash
-###############################################################################
-# firewalld : HALB built of HAProxy and Keepalived (idempotent)
-###############################################################################
-[[ $(whoami) == 'root' ]] || exit 11
+##################################################################
+# firewalld : Configure for HA Application Load Balancer (HALB)
+# - Idempotent
+#
+# ARGs: K8S_API_PORT  HALB_STATS_PORT  [ANY(to teardown)]
+##################################################################
+[[ "$(id -u)" -ne 0 ]] && {
+    echo "❌  ERR : MUST run as root" >&2
 
-[[ $(systemctl is-active firewalld.service) == 'active' ]] ||
-    systemctl enable --now firewalld.service
+    exit 11
+}
+[[ -n $2 ]] || {
+    echo "❌  ERR : Missing args" >&2
+
+    exit 22
+}
+## Assure firewalld.service is running; persist otherwise.
+systemctl is-active --quiet firewalld ||
+    systemctl enable --now firewalld
 
 k8s_port="$1"
 stats_port="$2"
-svc=halb
-zone=$(firewall-cmd --get-active-zone |head -n1)
-at="--permanent --zone=$zone --service=$svc"
+[[ -n $3 ]] &&
+    export do='remove' ||
+        export do='add'
 
-[[ $(firewall-cmd --get-services |grep $svc) ]] ||
-    firewall-cmd --permanent --zone=$zone --new-service=$svc
+svc='halb'
+zone="$(firewall-cmd --get-active-zone |head -n1)"
 
-firewall-cmd $at --set-description="HAProxy/Keepalived in VRRP mode"
-firewall-cmd $at --add-port=80/tcp
-firewall-cmd $at --add-port=443/tcp
-firewall-cmd $at --add-port=6443/tcp
-firewall-cmd $at --add-port=$k8s_port/tcp
-firewall-cmd $at --add-port=$stats_port/tcp
+[[ $do == 'add' ]] && {
+    ## Create and configure the service
 
-firewall-cmd --permanent --zone=$zone --add-service=$svc
+    firewall-cmd --get-services --zone=$zone |grep -q "\b$svc\b" ||
+        firewall-cmd --new-service=$svc
+
+    at="--permanent --zone=$zone --service=$svc"
+
+    firewall-cmd $at --set-description="HAProxy/Keepalived in VRRP mode"
+
+    firewall-cmd $at --$do-port=80/tcp
+    firewall-cmd $at --$do-port=443/tcp
+    firewall-cmd $at --$do-port=6443/tcp
+    firewall-cmd $at --$do-port=$k8s_port/tcp
+    firewall-cmd $at --$do-port=$stats_port/tcp
+}
+firewall-cmd --permanent --zone=$zone --$do-service=$svc
 
 at="--permanent --zone=$zone"
 
 ## Allow ICMP for ping request/reply : Inversion is *required* here when target is DROP 
-firewall-cmd $at --add-icmp-block-inversion    # Invert so block allows
-firewall-cmd $at --add-icmp-block=echo-request # block (allow) request 
-firewall-cmd $at --add-icmp-block=echo-reply   # block (allow) reply
+# firewall-cmd $at --$do-icmp-block-inversion    # Invert so block allows
+# firewall-cmd $at --$do-icmp-block=echo-request # block (allow) request 
+# firewall-cmd $at --$do-icmp-block=echo-reply   # block (allow) reply
 
 ## VRRP : Multicast
-firewall-cmd $at --add-rich-rule='rule family="ipv4" destination address="224.0.0.0/4" accept'
+firewall-cmd $at --$do-rich-rule='rule family="ipv4" destination address="224.0.0.0/4" accept'
 
 ## VRRP : Protocol 112 (an L3 protocol)
-#firewall-cmd $at --add-rich-rule='rule protocol value="vrrp" accept'
-#firewall-cmd $at --add-rich-rule='rule family="ipv4" source address="'$vip'" protocol value="vrrp" accept'
+#firewall-cmd $at --$do-rich-rule='rule protocol value="vrrp" accept'
+#firewall-cmd $at --$do-rich-rule='rule family="ipv4" source address="'$vip'" protocol value="vrrp" accept'
 # Add DIRECT RULEs for non-UDP/TCP protocols 
 # iptables -I INPUT -p 112 -j ACCEPT
 # iptables -I OUTPUT -p 112 -j ACCEPT
 firewall-cmd --permanent --direct \
-    --add-rule ipv4 filter INPUT 0 -p 112 -j ACCEPT
+    --$do-rule ipv4 filter INPUT 0 -p 112 -j ACCEPT
 firewall-cmd --permanent --direct \
-    --add-rule ipv4 filter OUTPUT 0 -p 112 -j ACCEPT
+    --$do-rule ipv4 filter OUTPUT 0 -p 112 -j ACCEPT
 
 ## VIP : Allow/Limit traffic to/from VIP address by either IPv4 or IPv6 
 #at="--permanent --zone=$zone"
-#firewall-cmd $at --add-rich-rule='rule family="ipv4" source address="'$vip'" accept'
-#firewall-cmd $at --add-rich-rule='rule family="ipv6" source address="'$vip6'" accept'
+#firewall-cmd $at --$do-rich-rule='rule family="ipv4" source address="'$vip'" accept'
+#firewall-cmd $at --$do-rich-rule='rule family="ipv6" source address="'$vip6'" accept'
 
 # Update firewalld.service sans restart 
 firewall-cmd --reload
 
-firewall-cmd --list-all --zone=$zone
+firewall-cmd --list-services --zone=$zone |grep -q "\b$svc\b" || {
+    echo "❌  ERR : $? : NO service '$svc' is listed in zone '$zone'" >&2
+
+    exit 99
+}
