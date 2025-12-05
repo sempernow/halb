ab59504b145834148fb8e072853b9dc901520ef8
dev <sempernow.com@gmail.com>
2025-12-05 14:17:31 -0500

    test-failover.sh @ 2025-12-05T19:17Z

--- test-failover.sh
+++ test-failover.sh
@@ -3,28 +3,39 @@
 # Test HALB failover 
 #################################################
 
-# @ nc : Verify connectivity
+# Verify connectivity
 test="nc -zvw2 $HALB_VIP $HALB_PORT_K8S $HALB_PORT_STATS https http"
 echo "🔍  Verify connectivity : $test"
 [[ $(type -t nc) ]] && {
     got="$($test 2>&1)" && echo -e "✅  ok\n$got" || echo "❌ $got"
 } || { echo "🧩  REQUIREs utility: nc"; }
 
-# @ ping : Verify HA (failover) dynamics
-[[ $(type -t ping) ]] && {
+echo
+
+# Verify HA 
+[[ $(type -t curl) ]] && {
+    echo '🧪  Verify LB failover (HA) dynamics.'
     echo '
-        Verify FAILOVER (HA) dynamics"
-        
-        While this ping test is running, reboot
-        or shutdown the Keepalive MASTER host. 👈
+      PRESS ENTER when ready to test. 
+      
+      Use CTRL+C to kill.
 
-        Connectivity should persist as long as 
-        at least one HALB host is running.
+      HOW TO:
 
-        PRESS ENTER when ready to test. 
-
-        Use CTRL+C to kill.
+      1. Find the Keepalived MASTER host. 
+         - The LB vIP is attached to its public interface.
+      2. Start this test, which spams the LB /stats endpoint.
+      3. Stop the systemd haproxy.service on the MASTER host, or reboot it.
+         - sudo systemctl stop haproxy.service  # Test host-process failover. 
+         - sudo reboot                          # Test host failover.
+   
+      HTTP 200 responses from LB to these test requests 
+      should persist as long as at least one LB host is active.
     '
     read
-    ping -4 -D $HALB_VIP
-} || echo "Use \`ping -4 -D $HALB_VIP\` to verify failover (HA) when keepalived MASTER node is offline."
+    seq 999999 |xargs -I{} /bin/bash -c '
+        curl -ksSIX GET http://$0/stats |grep HTTP
+        date -Is;sleep 5
+    ' "$HALB_VIP:$HALB_PORT_STATS" {} 
+} || echo "Use an HTTP client to spam HALB endpoint, http://$HALB_VIP:$HALB_PORT_STATS/stats, to verify failover (HA) when keepalived MASTER node is offline."
+
